4b0718af63ed69cf4a17d6f5d5666c81201c17d3
Danda-Lion <dwightdanda123@gmail.com>
2024-03-05 14:46:02 +0300

    userPost modified

--- src/main/java/com/example/demo/Models/UserPost.java
+++ src/main/java/com/example/demo/Models/UserPost.java
@@ -1,25 +1,47 @@
-//package com.example.demo.Models;
-//
-//import jakarta.persistence.*;
-//import lombok.Data;
-//
-//@Entity
-//@Data
-//@Table(name="userPosts")
-//public class UserPost {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "profileId", nullable = false)
-//    private Profile profile;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "countryId", nullable = false)
-//    private Country country;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "")
-//}
+package com.example.demo.Models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.security.access.method.P;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name="userPosts")
+public class UserPost {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "profileId", nullable = false)
+    private Profile profile;
+
+    @ManyToOne
+    @JoinColumn(name = "countryId", nullable = false)
+    private Country country;
+
+    @ManyToMany
+    @JoinTable(name = "userPostHashTag",joinColumns = @JoinColumn(name = "userPostId"),inverseJoinColumns = @JoinColumn(name = "hashTagId", nullable = false))
+    private List<HashTag> hashTags;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "brandId")
+    private Brand brand;
+
+    @Column
+    private String thumbnail;
+
+    @Column
+    private LocalDateTime time;
+
+    @Column
+    private String type;
+
+    @Column
+    private String path;
+
+}
