b07568a562526c651f6ee9a5d5884a354918cc6f
kingposhwolf <kingposhwolf@zohomail.com>
2024-03-18 10:32:43 +0300

    tags

--- src/main/java/com/example/demo/Models/Tag.java
+++ src/main/java/com/example/demo/Models/Tag.java
@@ -1,26 +1,22 @@
 package com.example.demo.Models;
 
 import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import lombok.Data;
 
-@Entity
-@Data
-public class FollowUnFollow {
+public class Tag {
     @Id
-    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "followerId", nullable = false)
-    private Profile follower;
+    @JoinColumn(name = "profileId", nullable = false)
+    private Profile user;
 
     @ManyToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "followingId", nullable = false)
-    private Profile following;
+    @JoinColumn(name = "userPostId", nullable = false)
+    private UserPost post;
 }
