a86f6239ab7c3e984f39310626c7078b579102c2
Danda-Lion <dwightdanda123@gmail.com>
2024-03-19 15:05:47 +0300

    this new post is by re-writing the bytesd

---
+++ src/main/java/com/example/demo/Models/Tag.java
@@ -0,0 +1,26 @@
+package com.example.demo.Models;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Data
+@Entity
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "profileId", nullable = false)
+    private Profile user;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "userPostId", nullable = false)
+    private UserPost post;
+}
