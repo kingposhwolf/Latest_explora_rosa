3425e130185532d1919e7a90e8fc2ce35e91f8d5
kingposhwolf <kingposhwolf@zohomail.com>
2024-03-27 14:58:10 +0300

    Chatting changes

--- src/main/java/com/example/demo/userChat/Status2.java
+++ src/main/java/com/example/demo/userChat/Status2.java
@@ -1,4 +1,4 @@
-package com.example.demo.user;
+package com.example.demo.userChat;
 
 public enum Status2 {
     ONLINE, OFFLINE
--- src/main/java/com/example/demo/userChat/User2.java
+++ src/main/java/com/example/demo/userChat/User2.java
@@ -1,4 +1,4 @@
-package com.example.demo.user;
+package com.example.demo.userChat;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
