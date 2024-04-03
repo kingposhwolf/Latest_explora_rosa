4f09b663ee8e4733205cc974116c1bc6a717ee0e
kingposhwolf <kingposhwolf@zohomail.com>
2024-04-03 22:20:49 +0300

    file sharing

--- src/main/java/com/example/demo/Services/UserPostService/UserPostServiceImpl.java
+++ src/main/java/com/example/demo/Services/UserPostService/UserPostServiceImpl.java
@@ -21,14 +21,12 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 @Service
