11c8876a76b9d44a5cb813b989b398ddccaf9a99
kingposhwolf <kingposhwolf@zohomail.com>
2024-04-07 14:42:49 +0300

    Like operation complete

--- src/main/java/com/example/demo/Models/SocialMedia/Interactions/Like.java
+++ src/main/java/com/example/demo/Models/SocialMedia/Interactions/Like.java
@@ -8,6 +8,7 @@ import com.example.demo.Models.UserManagement.Profile;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.ToString;
 
 @Data
 @Entity
@@ -25,6 +26,7 @@ public class Like {
 
     @ManyToOne
     @JoinColumn(name = "userPostId", nullable = false, foreignKey = @ForeignKey(name = "FK_POST_LIKE", foreignKeyDefinition = "FOREIGN KEY (user_post_id) REFERENCES user_posts(id) ON DELETE CASCADE"))
+    @ToString.Exclude
     private UserPost post;
 
     private boolean deleted = Boolean.FALSE;
--- src/main/java/com/example/demo/Repositories/LikeRepository.java
+++ src/main/java/com/example/demo/Repositories/LikeRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.Repositories;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
@@ -19,4 +20,8 @@ public interface LikeRepository extends CrudRepository<Like, Long>{
 
     @Query(value = "SELECT * FROM `like` WHERE user_post_id = :postId AND profile_id = :likerId AND deleted = true", nativeQuery = true)
     Optional<Like> findDeletedLikesByPostAndLiker(@Param("postId") Long postId, @Param("likerId") Long likerId);
+
+    @Query("SELECT DISTINCT l.liker.id as profileId, l.liker.verificationStatus as verificatioStatus, l.liker.user.username as username, l.liker.user.name as name FROM Like l JOIN l.liker u WHERE l.post.id = :postId ")
+    List<Map<String, Object>> findLikersByUsernameAndName(@Param("postId") Long postId);
+
 }
