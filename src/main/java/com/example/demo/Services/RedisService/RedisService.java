8e235c4f7964171c0e74f8203d234d37f7777316
kingposhwolf <kingposhwolf@zohomail.com>
2024-05-20 19:29:31 +0300

    redis configuration one

---
+++ src/main/java/com/example/demo/Services/RedisService/RedisService.java
@@ -0,0 +1,20 @@
+package com.example.demo.Services.RedisService;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class RedisService {
+    private RedisTemplate<String, Object> redisTemplate;
+
+    public void saveDataWithDynamicExpiration(String key, Object value, Duration expiration) {
+        ValueOperations<String, Object> valueOps = redisTemplate.opsForValue();
+        valueOps.set(key, value, expiration);
+    }
+}
